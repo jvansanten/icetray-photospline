44c762f21c225a1ac54bed2b5d3fd4fb0260ad54
Nathan Whitehorn <nwhitehorn@icecube.wisc.edu>
2009-02-04 21:09:53 +0000

    Use bspline.splinebasis in the spline fit example.
    git-svn-id: http://code.icecube.wisc.edu/svn/sandbox/nwhitehorn/photospline@52335 16731396-06f5-0310-8873-f7f720988828

--- fitter/bsplinefit.py
+++ fitter/bsplinefit.py
@@ -10,13 +10,7 @@ order=2
 x1 = numpy.sort(numpy.random.uniform(0,25,size=numpts))
 z = numpy.random.poisson(numpy.cos(x1)*numpy.cos(x1) + (x1 -3.0)*(x1-3.0) + 10)
 
-splinevals = []
-i = 0
-while i < len(knots)-order-1:
-	splinevals.append([bspline(knots,x,i,order) for x in x1])
-	i = i+1
-
-A = numpy.column_stack(splinevals)
+A = splinebasis(knots,order,x1)
 result = numpy.linalg.lstsq(A, z)
 
 gp = Gnuplot.Gnuplot()
