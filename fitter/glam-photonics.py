eeb9bb809652a6610526f1eac9c869cea05d5597
Nathan Whitehorn <nwhitehorn@icecube.wisc.edu>
2009-03-01 17:23:57 +0000

    Fix syntax error.
    git-svn-id: http://code.icecube.wisc.edu/svn/sandbox/nwhitehorn/photospline@52943 16731396-06f5-0310-8873-f7f720988828

--- fitter/glam-photonics.py
+++ fitter/glam-photonics.py
@@ -1,18 +1,19 @@
 from glam import glam, splinefitstable
 import numpy
 import sys
+import os
 
 # Hard-coded params
 
-nknots = 25
-smooth = 0.2
+nknots = 30
+smooth = 0.1
 
 # Real code
 
 rawdata = numpy.loadtxt(sys.argv[1])
-rawdata = rawdata[rawdata[:,1] == 45]
+#rawdata = rawdata[rawdata[:,1] == 45]
 z = rawdata[:,6]
-data = rawdata[:,[0,2,5]]
+data = rawdata[:,[0,1,2]]
 ndim = 3
 ranges = numpy.column_stack((data.min(0),data.max(0)))
 knots = []
@@ -21,7 +22,7 @@ print "Axis lengths:"
 for r in ranges:
 	print "\t",r[0],"-",r[1]
 	space = (r[1] - r[0])/nknots
-	knots.append(numpy.linspace(r[0]-3.5*space,r[1]+2*space,nknots))
+	knots.append(numpy.linspace(r[0]-3.9*space,r[1]+2.5*space,nknots))
 	periods.append(0)
 
 munge = [numpy.unique(data[:,i]) for i in range(0,3)]
@@ -34,11 +35,18 @@ z[numpy.isinf(z)] = 0
 
 print "Loaded histogram with dimensions ",z.shape
 
+outputfile = sys.argv[1]+".pspl.fits";
+if os.path.exists(outputfile):
+	if raw_input("File %s exists. Overwrite? (y/n)" % outputfile) == 'y':
+		os.unlink(outputfile)
+	else:
+		sys.exit()
+
 print "Beginning spline fit..."
 table = glam.fit(z,w,munge,knots,2,smooth,periods)
 
-print "Saving table to %s..." % (sys.argv[1]+".pspl")
-splinefitstable.write(table,sys.argv[1]+".pspl.fits")
+print "Saving table to %s..." % outputfile
+splinefitstable.write(outputfile)
 
 smoothed = glam.grideval(table,munge)
 
