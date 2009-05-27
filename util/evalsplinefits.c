a3394fa40d195c7e13ef4fc5ed17c8e217e96563
Nathan Whitehorn <nwhitehorn@icecube.wisc.edu>
2009-05-27 13:06:09 +0000

    Provide evaluation time statistics. It seems that fixing the table bin center search to be smarter is not a useful optimization (1 us vs. 13 us for the actual evaluation on my laptop).
    git-svn-id: http://code.icecube.wisc.edu/svn/sandbox/nwhitehorn/photospline@55367 16731396-06f5-0310-8873-f7f720988828

--- util/evalsplinefits.c
+++ util/evalsplinefits.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <sys/time.h>
 
 #include "splinetable.h"
 #include "bspline.h"
@@ -10,16 +11,28 @@ void usage() {
 	exit(1);
 }
 
+#define TIMING
+
 int main(int argc, char **argv) {
 	struct splinetable table;
 	int i;
-	double *x;
+	double *x, value;
 	int *centers;
+	struct timeval tp1, tp2;
 
 	if (argc < 2)
 		usage();
 
+	gettimeofday(&tp1, NULL);
 	readsplinefitstable(argv[1], &table);
+	gettimeofday(&tp2, NULL);
+
+    #ifdef TIMING
+	if (tp2.tv_usec < tp1.tv_usec)
+		tp2.tv_usec += 1e6;
+	printf("Time to open table: %ld.%06ld seconds\n", 
+	    tp2.tv_sec - tp1.tv_sec, tp2.tv_usec - tp1.tv_usec);
+    #endif
 
 	if (argc < 2+table.ndim)
 		usage();
@@ -28,12 +41,31 @@ int main(int argc, char **argv) {
 	centers = malloc(sizeof(double)*table.ndim);
 	for (i = 0; i < table.ndim; i++) 
 		sscanf(argv[i+2],"%lf",&x[i]);
+	gettimeofday(&tp1, NULL);
 	tablesearchcenters(&table, x, centers);
+	value = ndsplineeval(&table, x, centers);
+	gettimeofday(&tp2, NULL);
+
+    #ifdef TIMING
+	if (tp2.tv_usec < tp1.tv_usec)
+		tp2.tv_usec += 1e6;
+	printf("Cold cache evaluation time: %ld microseconds\n",
+	    tp2.tv_usec - tp1.tv_usec);
+
+	gettimeofday(&tp1, NULL);
+	value = ndsplineeval(&table, x, centers);
+	gettimeofday(&tp2, NULL);
+
+	if (tp2.tv_usec < tp1.tv_usec)
+		tp2.tv_usec += 1e6;
+	printf("Warm cache evaluation time: %ld microseconds\n",
+	    tp2.tv_usec - tp1.tv_usec);
+    #endif
 
 	printf("NDim: %d\n",table.ndim);
 	printf("Order: %d\n",table.order);
 
-	printf("Value: %lf\n",ndsplineeval(&table, x, centers));
+	printf("Value: %lf\n",value);
 
 	return 0;
 }
