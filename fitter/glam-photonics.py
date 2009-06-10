f9cac9d438e0e9b00dbb3f445077e253b51e5249
Nathan Whitehorn <nwhitehorn@icecube.wisc.edu>
2009-06-10 11:58:43 +0000

    Add support for replacing table zeros with a low-weighted epsilon using the new '-e' options to glam-photonics.
    git-svn-id: http://code.icecube.wisc.edu/svn/sandbox/nwhitehorn/photospline@55734 16731396-06f5-0310-8873-f7f720988828

--- fitter/glam-photonics.py
+++ fitter/glam-photonics.py
@@ -1,14 +1,22 @@
-#from glam import glam, splinefitstable
 from glam import splinefitstable
+from optparse import OptionParser
 import numpy
 import sys
 import os
 
 # Hard-coded params
 
-nknots = [13, 7, 15, 20] # [r, phi, z, t]
+nknots = [13, 5, 15, 20] # [r, phi, z, t]
 #nknots = [9, 7, 10, 13] # [r, phi, z, t]
-smooth = 1e-4
+smooth = 100
+
+# Parse arguments
+
+usage = "usage: %prog [options] table.pt [output.fits]"
+optparser = OptionParser(usage=usage)
+optparser.add_option("-e", "--epsilon", dest="epsilon", type="float",
+		     help="epsilon with which to replace table zeros")
+(opts, args) = optparser.parse_args()
 
 # Real code
 
@@ -23,7 +31,7 @@ except:
 try:
 	import photo2numpy
 
-	table = photo2numpy.readl1(sys.argv[1])
+	table = photo2numpy.readl1(args[0])
 	z = table[0]
 	if table[1] == None:
 		weights = numpy.ones(z.shape)
@@ -39,19 +47,20 @@ try:
 	# NB: we can only do this with photo2numpy
 	# NB: this is a disgusting hack
 
-	if sys.argv[1].endswith(".prob"):
+	if args[0].endswith(".prob"):
+		print "Input table ends in .prob -- attempting to recover PDF"
 		first_slice = [slice(None)]*(len(z.shape)-1) + [slice(0,1)]
 		z = numpy.append(z[first_slice],numpy.diff(z,axis=-1),axis=-1) / bin_widths[-1]
 
 except:
 	print "Using photo2numpy failed, falling back on text processing...\n"
 
-	data = numpy.loadtxt(sys.argv[1])
+	data = numpy.loadtxt(args[0])
 	data = data[:,[0,1,2,5,6]]
 	z = data[:,4]
 	ndim = 4
 	try:
-		weights = numpy.loadtxt(sys.argv[1] + ".stats")[:,6]
+		weights = numpy.loadtxt(args[0] + ".stats")[:,6]
 	except:
 		weights = numpy.ones(z.shape)
 	# Get coordinates
@@ -77,6 +86,7 @@ zknots = numpy.append(numpy.append([-800, -700, -600],coreknots[2]),[600,700,800
 
 if ndim==4:
 	tknots = numpy.append(numpy.append([-1,-0.5,0],coreknots[3]),[7100, 7150, 7200, 7300, 7400])
+	tknots = numpy.sort(numpy.append([1, 5, 10],tknots))
 	periods = [0,0,0,0]
 	knots = [rknots, thetaknots, zknots, tknots]
 else:
@@ -90,18 +100,24 @@ print 'Number of knots used: ',[len(a) for a in knots]
 bin_centers[1][0] = 0
 bin_centers[1][bin_centers[1].size - 1] = 180
 
-# Convert the input to log-space and drop any NaNs or infinites from the fit
-z = numpy.log(z)
+# Set up weights and data array
 w = weights
+
+# Convert the input to log-space and drop any NaNs or infinites from the fit
+if opts.epsilon != None:
+	w[z == 0] = 0.01
+	z = z + opts.epsilon
+
+z = numpy.log(z)
 w[numpy.logical_not(numpy.isfinite(z))] = 0
 z[numpy.logical_not(numpy.isfinite(z))] = 0
 
 print "Loaded histogram with dimensions ",z.shape
 
-if len(sys.argv) < 3:
-	outputfile = sys.argv[1]+".pspl.fits"
+if len(args) < 3:
+	outputfile = args[0]+".pspl.fits"
 else:
-	outputfile = sys.argv[2]
+	outputfile = args[1]
 
 if os.path.exists(outputfile):
 	if raw_input("File %s exists. Overwrite? (y/n)" % outputfile) == 'y':
