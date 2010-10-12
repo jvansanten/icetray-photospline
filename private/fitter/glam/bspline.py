942c712d2bd5798e3a7500ea0124948464184ba1
Jakob van Santen <jvansanten@icecube.wisc.edu>
2010-10-12 22:43:53 +0000

    Python implementations of BSPLVB and friends.
    git-svn-id: http://code.icecube.wisc.edu/svn/projects/photospline/trunk@67472 16731396-06f5-0310-8873-f7f720988828

--- private/fitter/glam/bspline.py
+++ private/fitter/glam/bspline.py
@@ -30,6 +30,68 @@ def bspline(knots, x, i, n):
 	a = (x - knots[i])*bspline(knots, x, i, n-1)/(knots[i+n] - knots[i])
 	b = (knots[i+n+1] - x)*bspline(knots, x, i+1, n-1)/(knots[i+n+1] - knots[i+1])
 	return a+b
+	
+def bspline_nonzero(knots, x, n):
+	"""Calculate the value of the possibly non-zero b-splines at x"""
+	left = numpy.digitize([x], knots)[0] - 1
+	jhigh = n + 1
+	biatx = numpy.zeros(jhigh)
+	delta_l = numpy.zeros(jhigh)
+	delta_r = numpy.zeros(jhigh)
+	bsplvb(knots, x, left, 0, jhigh, biatx, delta_l, delta_r)
+	return biatx
+	
+def bspline_deriv_nonzero(knots, x, n):
+	"""Calculate the value of the possibly non-zero b-spline derivatives at x"""
+	left = numpy.digitize([x], knots)[0] - 1
+	jhigh = n
+	biatx = numpy.zeros(jhigh+1)
+	# special case: everything's constant
+	if n == 0:
+		return biatx
+	delta_l = numpy.zeros(jhigh)
+	delta_r = numpy.zeros(jhigh)
+	# calculate the values of the n-1th order B-splines at x
+	bsplvb(knots, x, left, 0, n, biatx, delta_l, delta_r)
+	# post-process to form the 1st derivatives of each nth order B-spline
+	temp = biatx[0]
+	# biatx[0] contains the first nonzero n-1th order B-spline
+	# On the last supported interval of the ith nth order
+	# spline, only the i+1th order n-1th order spline is nonzero.
+	biatx[0] =  - n*temp / ((knots[left+n+1] - knots[left+1]))
+	# now, both the ith and i+1th n-1th order splines contribute
+	for i in xrange(1, n):
+		a = n*temp/((knots[left+i+n] - knots[left+i]))
+		b = n*biatx[i]/(knots[left+i+n+1] - knots[left+i+1])
+		temp = biatx[i]
+		biatx[i] = a-b
+	# only the ith n-1th order spline is nonzero on the first supported interval
+	biatx[n] = n*temp/((knots[left+n+n-1] - knots[left+n-1]))
+	return biatx
+	
+def bsplvb(knots, x, left, jlow, jhigh, biatx, delta_l, delta_r):
+	"""Braindead reimplementation of de Boor's BSPLVB
+	(an algorithm for generating the non-zero B-splines from the bottom up
+	without unnecessarily re-calculating terms)"""
+	
+	if jlow == 0:
+		biatx[0] = 1.0
+	
+	for j in xrange(jlow, jhigh - 1):
+		delta_r[j] = knots[left+j+1] - x
+		if left-j < 0:
+			delta_l[j] = 0
+		else:
+			delta_l[j] = x - knots[left-j]
+		
+		
+		saved = 0.0
+		
+		for i in xrange(j+1):
+			term = biatx[i] / (delta_r[i] + delta_l[j-i])
+			biatx[i] = saved + delta_r[i]*term
+			saved = delta_l[j-i]*term
+		biatx[j+1] = saved
 
 def bspline_deriv(knots, x, i, n):
 	if n == 0:
