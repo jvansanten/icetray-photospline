268512f384c7d613406b0a2ed8718197f6380f17
Jakob van Santen <jvansanten@icecube.wisc.edu>
2010-10-20 03:37:56 +0000

    Rejigger setup script to allow extensions to be disabled.
    git-svn-id: http://code.icecube.wisc.edu/svn/projects/photospline/trunk@67697 16731396-06f5-0310-8873-f7f720988828

--- private/setup.py
+++ private/setup.py
@@ -1,4 +1,4 @@
-from distutils.core import setup, Extension
+from setuptools import setup, Feature, Extension
 from numpy.distutils.system_info import get_info
 import sys, os
 
@@ -28,47 +28,51 @@ else:
 
 expand = lambda path: os.path.expanduser(os.path.expandvars(path))
 
-def setup_extensions():
-	if cp.has_option('photonics','path'):
-		# We a complete distribution of photonics, including the level2
-		# headers. Build with this one.
-		photonics_dir = expand(cp.get('photonics','path'))
-		photo2numpy = Extension("photo2numpy",
-		    sources=["photo2numpy/photo2numpy.c"],
-		    include_dirs=inc_dirs + [photonics_dir+'/lib',
-		        photonics_dir+'/level2'],
-		    extra_objects=[photonics_dir+'/lib/.libs/libphotonics.a',
-		        photonics_dir+'/level2/.libs/liblevel2amasim.a'],
-		    libraries = ['m'] )
-	elif os.path.isdir(photonics_libdir):
-		# We only have the I3 ports install of photonics, which is
-		# missing level2_reader.h. Skip the bits that read l2 tables.
-		photonics_libs = ['photonics']
-		objs = [photonics_libdir + ('/lib%s.a' % lib) 
-		    for lib in photonics_libs]
-		photo2numpy = Extension("photo2numpy",
-		    sources=["photo2numpy/photo2numpy.c"],
-		    include_dirs = inc_dirs + [photonics_incdir],
-		    extra_objects = objs,
-		    define_macros = [('SKIP_LEVEL2',None)],
-		    libraries = ['m'] )
-	else:
-		print "Couldn't find photonics! Skipping build of photo2numpy."
-		photo2numpy = None
-	
-	spglam = Extension("spglam",
-	    sources=["cfitter/glam.c", "cfitter/nnls.c", 
-	        "cfitter/cholesky_solve.c", "cfitter/splineutil.c", 
-	        "cfitter/pyglam.c","lib/bspline.c"],
-	    include_dirs=inc_dirs + ['../public'],
-	    libraries = ['m', 'cholmod', 'ccolamd', 'colamd', 'amd', 'camd', 
-	        'metis', 'spqr','gfortran','stdc++'] + lapack_libs,
-	    undef_macros = ['NDEBUG'] )
-	
-	if photo2numpy is not None:
-		return [photo2numpy, spglam]
-	else:
-		return [spglam]
+if cp.has_option('photonics','path'):
+	# We a complete distribution of photonics, including the level2
+	# headers. Build with this one.
+	photonics_dir = expand(cp.get('photonics','path'))
+	photo2numpy = Extension("photo2numpy",
+	    sources=["photo2numpy/photo2numpy.c"],
+	    include_dirs=inc_dirs + [photonics_dir+'/lib',
+	        photonics_dir+'/level2'],
+	    extra_objects=[photonics_dir+'/lib/.libs/libphotonics.a',
+	        photonics_dir+'/level2/.libs/liblevel2amasim.a'],
+	    libraries = ['m'] )
+elif os.path.isdir(photonics_libdir):
+	# We only have the I3 ports install of photonics, which is
+	# missing level2_reader.h. Skip the bits that read l2 tables.
+	photonics_libs = ['photonics']
+	objs = [photonics_libdir + ('/lib%s.a' % lib) 
+	    for lib in photonics_libs]
+	photo2numpy = Extension("photo2numpy",
+	    sources=["photo2numpy/photo2numpy.c"],
+	    include_dirs = inc_dirs + [photonics_incdir],
+	    extra_objects = objs,
+	    define_macros = [('SKIP_LEVEL2',None)],
+	    libraries = ['m'] )
+else:
+	print "Couldn't find photonics! Skipping build of photo2numpy."
+	photo2numpy = None
+		
+spglam = Extension("spglam",
+    sources=["cfitter/glam.c", "cfitter/nnls.c", 
+        "cfitter/cholesky_solve.c", "cfitter/splineutil.c", 
+        "cfitter/pyglam.c","lib/bspline.c"],
+    include_dirs=inc_dirs + ['../public'],
+    libraries = ['m', 'cholmod', 'ccolamd', 'colamd', 'amd', 'camd', 
+        'metis', 'spqr','gfortran','stdc++'] + lapack_libs,
+    undef_macros = ['NDEBUG'] )
+
+spglam_feature = Feature('C version of GLAM (requires SuiteSparse)',
+    standard=True, ext_modules=[spglam])
+
+features = {'spglam': spglam_feature}
+
+if photo2numpy is not None:
+	photo2numpy_feature = Feature('Photonics table support',
+	    standard=True, ext_modules=[photo2numpy])
+	features['photo2numpy'] = photo2numpy_feature
 
 def setup_package():
 	setup(name = 'photospline', version = '1e-12',
@@ -76,7 +80,7 @@ def setup_package():
 	    package_dir = {'glam': 'fitter/glam',
 	         'pyphotonics': 'fitter/pyphotonics'},
 	    packages = ["glam","pyphotonics"],
-	    ext_modules = setup_extensions()
+	    features = features
 	)
 
 if __name__ == "__main__":
