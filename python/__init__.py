752066a53c76528631b12415f91e4d502f6e1bb8
Nathan Whitehorn <nwhitehorn@icecube.wisc.edu>
2011-05-10 18:49:33 +0000

    Add splinefitstable here.
    git-svn-id: http://code.icecube.wisc.edu/svn/projects/photospline/trunk@74951 16731396-06f5-0310-8873-f7f720988828

--- python/__init__.py
+++ python/__init__.py
@@ -2,6 +2,7 @@ __all__=['glam', 'spglam', 'photo2numpy', 'pyphotonics', 'splinetable']
 
 import glam
 import splinetable
+import splinefitstable
 import spglam
 import numpy_extensions
 import photo2numpy
