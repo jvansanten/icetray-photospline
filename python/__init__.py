f66bee01029e75b2eef01b1958ae57ea9268f5bf
Nathan Whitehorn <nwhitehorn@icecube.wisc.edu>
2011-11-28 19:44:36 +0000

    Handle missing numpy more gracefully.
    git-svn-id: http://code.icecube.wisc.edu/svn/projects/photospline/trunk@82320 16731396-06f5-0310-8873-f7f720988828

--- python/__init__.py
+++ python/__init__.py
@@ -3,14 +3,17 @@ __all__=['glam', 'spglam', 'photo2numpy', 'pyphotonics', 'splinetable']
 from icecube.load_pybindings import load_pybindings
 load_pybindings(__name__, __path__)
 
-import glam
-import splinetable
-import splinefitstable
 try:
-	import spglam
+	import splinetable
+	import splinefitstable
+	import glam
+	try:
+		import spglam
+	except ImportError:
+		pass
+	import numpy_extensions
+	import photo2numpy
+	import pyphotonics
 except ImportError:
 	pass
-import numpy_extensions
-import photo2numpy
-import pyphotonics
 
