c6859552e765e4f6ddda09e86169903fa6a2118a
Nathan Whitehorn <nwhitehorn@icecube.wisc.edu>
2012-01-07 18:13:56 +0000

    Remove pointless boost code and fix compilation with clang.
    git-svn-id: http://code.icecube.wisc.edu/svn/projects/photospline/trunk@83620 16731396-06f5-0310-8873-f7f720988828

--- private/lib/I3SplineTable.cxx
+++ private/lib/I3SplineTable.cxx
@@ -1,36 +1,31 @@
-
 #include <stdexcept>
-#include "photospline/I3SplineTable.h"
-extern "C" {
-#include "photospline/bspline.h"
-}
+#include <photospline/I3SplineTable.h>
 
-static void
-splinetable_destructor(splinetable *table) {
-	if (!table) return;
-	
-	splinetable_free(table);
-	delete table;
+extern "C" {
+	#include <photospline/bspline.h>
 }
 
 I3SplineTable::I3SplineTable(const std::string &path)
 {
-	table_ = boost::shared_ptr<splinetable>(new splinetable,
-            splinetable_destructor);
-
-	if (readsplinefitstable(path.c_str(), &*table_) != 0)
+	if (readsplinefitstable(path.c_str(), &table_) != 0)
 		throw std::runtime_error("Couldn't read spline table " + path);
+}
+
+I3SplineTable::~I3SplineTable()
+{
+	splinetable_free(&table_);
 }
 
 int
 I3SplineTable::Eval(double *coordinates, double *result)
 {
-	int centers[table_->ndim];
+	int centers[table_.ndim];
 	
-	if (tablesearchcenters(&*table_, coordinates, centers) == 0)
-		*result = ndsplineeval(&*table_, coordinates, centers, 0);
+	if (tablesearchcenters(&table_, coordinates, centers) == 0)
+		*result = ndsplineeval(&table_, coordinates, centers, 0);
 	else
 		return EINVAL;
 	
 	return 0;
 }
+
--- public/photospline/I3SplineTable.h
+++ public/photospline/I3SplineTable.h
@@ -1,14 +1,16 @@
-
 #include <string>
+
 extern "C" {
-#include <photospline/splinetable.h>
+	#include <photospline/splinetable.h>
 }
 
 class I3SplineTable {
 public:
 	I3SplineTable(const std::string &path);
-	virtual ~I3SplineTable() {};
+	virtual ~I3SplineTable();
+
 	int Eval(double *x, double *result);
-protected:
-	boost::shared_ptr<splinetable> table_;	
+
+private:
+	struct splinetable table_;
 };
