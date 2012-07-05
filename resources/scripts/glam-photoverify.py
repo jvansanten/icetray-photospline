10ac3a440f4914e21ec235a97b39bb91bacb02d7
Jakob van Santen <jvansanten@icecube.wisc.edu>
2012-07-05 14:44:37 +0000

    Make glam-photoverify plot evaluated PDFs in colors that are actually distinguishable. Also, switch the minority tabs to spaces.
    git-svn-id: http://code.icecube.wisc.edu/svn/projects/photospline/trunk@90329 16731396-06f5-0310-8873-f7f720988828

--- resources/scripts/glam-photoverify.py
+++ resources/scripts/glam-photoverify.py
@@ -32,16 +32,16 @@ optparser.add_option("-3", "--dim3", dest="dim3", type="string",
                      i    : iterate over this dimension                                            
                      <bin>: slice at this bin number''')
 optparser.add_option("--start",dest="start",type="int",default=0,
-		help='Start iterating at this bin number')
+        help='Start iterating at this bin number')
 optparser.add_option("--density",dest="density",type="int",default=1,
-		help="Points to plot per table bin")
+        help="Points to plot per table bin")
 optparser.add_option("--dump",dest="dump",action="store_true",default=False,
-		help="Iterate over dimension, dumping each plot to an EPS file.")
+        help="Iterate over dimension, dumping each plot to an EPS file.")
 (opts, args) = optparser.parse_args()
 
 if len(args) < 2:
-	print(usage)
-	sys.exit(1)
+    print(usage)
+    sys.exit(1)
 
 # Load original table
 table = Table(args[0], mmap=False)
@@ -110,11 +110,11 @@ timing = len(spline.knots) > 3
 
 for i,icenter in enumerate(table.bin_centers[idim]):
     if i < opts.start:
-    	continue
+        continue
     title = "Slice at %s = %f" % (axis_vars[idim], icenter)
     try:
-	slices = [slice(None)]*4
-	slices[idim] = i
+        slices = [slice(None)]*4
+        slices[idim] = i
         for d in free_axes:
             if axes[d] is not None:
                 bin = int(axes[d])
@@ -122,13 +122,13 @@ for i,icenter in enumerate(table.bin_centers[idim]):
                 bin = int(len(table.bin_centers[d])/2.)
             slices[d] = bin
             title += ", %s = %f" % (axis_vars[d], table.bin_centers[d][bin])
-	print "Building data set..."
-	if len(splines) == 1:
-	    sample = TableSlice(table, spline, slices, opts.density).flatten()
-	else:
-	    samples = [TableSlice(table, spline, slices, opts.density).flatten() for spline in splines]
-	    datacols = 2 if timing else 1
-	    stacks = tuple([samples[0]] + [sample[:,-datacols:] for sample in samples[1:]])
+    print "Building data set..."
+    if len(splines) == 1:
+        sample = TableSlice(table, spline, slices, opts.density).flatten()
+    else:
+        samples = [TableSlice(table, spline, slices, opts.density).flatten() for spline in splines]
+        datacols = 2 if timing else 1
+        stacks = tuple([samples[0]] + [sample[:,-datacols:] for sample in samples[1:]])
             sample = numpy.column_stack(stacks)
 
         gp.xlabel(axis_labels[xdim])
@@ -137,9 +137,9 @@ for i,icenter in enumerate(table.bin_centers[idim]):
 
         ndim = table.ndim
         if timing:
-		offset = 2
+            offset = 2
         else:
-		offset = 1
+            offset = 1
 
         if three_d:
             pass
@@ -154,26 +154,31 @@ for i,icenter in enumerate(table.bin_centers[idim]):
             gp.zlabel("CDF")
             gp.splot(*plots)
         else:
-
-            for plot in range(ndim+offset,sample.shape[1],offset):
+            showpdf = timing and xdim == 3
+            for cidx, plot in enumerate(range(ndim+offset,sample.shape[1],offset)):
+                if showpdf:
+                    ccolor = cidx*2 + 1
+                    pcolor = ccolor + 1
+                else:
+                    ccolor = cidx + 1
                 plots.append(Gnuplot.Data(sample[:,xdim],
                                           sample[:,plot],
-                                          title="%s CDF" % fit_labels[(plot - ndim - offset)/offset], axes='x1y1', with_='lines'))
-		if timing and xdim == 3:
-                	plots.append(Gnuplot.Data(sample[:,xdim],
+                                          title="%s CDF" % fit_labels[(plot - ndim - offset)/offset], axes='x1y1', with_='lines lc %d' % ccolor))
+                if showpdf:
+                    plots.append(Gnuplot.Data(sample[:,xdim],
                                           sample[:,plot+1],
-                                          title="%s PDF" % fit_labels[(plot - ndim - offset)/offset], axes='x1y2', with_='lines lc 3'))
+                                          title="%s PDF" % fit_labels[(plot - ndim - offset)/offset], axes='x1y2', with_='lines lc %d' % pcolor))
             plots.append(Gnuplot.Data(sample[:,xdim], sample[:,ndim],   title="Raw CDF", axes='x1y1', with_='points lc 1', every=opts.density))
-            if timing and xdim == 3:
-            	plots.append(Gnuplot.Data(sample[:,xdim], sample[:,ndim+1], title="Raw PDF", axes='x1y2', with_='points lc 3', every=opts.density))
+            if showpdf:
+                plots.append(Gnuplot.Data(sample[:,xdim], sample[:,ndim+1], title="Raw PDF", axes='x1y2', with_='points lc 3', every=opts.density))
             if timing:
                 ylabel = "CDF"
                 gp.ylabel(ylabel)
                 ylabel = "PDF"
                 gp("set y2label '%s'"%ylabel)
-		#gp("set y2tics 0, 0.02")
-		#gp("set y2range [*:*]")
-		#gp("set ytics nomirror")
+                #gp("set y2tics 0, 0.02")
+                #gp("set y2range [*:*]")
+                #gp("set ytics nomirror")
             else:
                 gp.ylabel('log Amplitude [PE/m track]')
             
@@ -188,14 +193,14 @@ for i,icenter in enumerate(table.bin_centers[idim]):
                 gp('set arrow from %e,%e to %e,%e as 2'%(k,ymin,k,ymax))
             gp.plot(*plots)
 
-	printdiffstats(sample[::opts.density,ndim], sample[::opts.density,ndim+offset])
-	if opts.dump:
-		if not three_d:
-            		gp.set(yrange=(0,1.2))
-			gp.set_boolean('grid',True)
-		gp.hardcopy('cdf_slice_%s_%.3d_%s_%f.eps'%(axis_vars[idim], i, axis_vars[d], table.bin_centers[d][bin]),eps=True,color=True)
-	else:
-		gp.interact()
+    printdiffstats(sample[::opts.density,ndim], sample[::opts.density,ndim+offset])
+    if opts.dump:
+        if not three_d:
+            gp.set(yrange=(0,1.2))
+            gp.set_boolean('grid',True)
+        gp.hardcopy('cdf_slice_%s_%.3d_%s_%f.eps'%(axis_vars[idim], i, axis_vars[d], table.bin_centers[d][bin]),eps=True,color=True)
+    else:
+        gp.interact()
         #raw_input('Press Enter to continue')
     except AssertionError:
         print 'Skipping   slice at  (no raw data)' % i
